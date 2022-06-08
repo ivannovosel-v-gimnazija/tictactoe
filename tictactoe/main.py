081b978c52ba0a1fd765f04392fcb04ec74c62c0
MartaKovacevic <106660870+MartaKovacevic@users.noreply.github.com>
2022-06-08 16:10:08 +0200

    Update main.py

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -18,19 +18,27 @@ while potez <= 9:
   
   # unos poteza nekog igrača
 	if potez % 2 == 0:
-		igrač = 'O'
-	else:
-		igrač = 'X'
-	unos = input('\n'+igrač+' unesi svoj potez u obliku x,y:').split(',')
-	x,y = int(unos[0])-1,int(unos[1])-1
-	if (x,y) not in zauzeti:
-		polje[x][y] = igrač
-		zauzeti.add((x,y))
-		potez += 1
-	else:
-		print('Ovo mjesto je zauzeto, unesi novu koordinatu')
-		continue
-    
+        igrač = 'O'
+    else:
+        igrač = 'X'
+    unos = input('\n'+igrač+' unesi svoj potez u obliku x,y:').split(',')
+    x,y = int(unos[0])-1,int(unos[1])-1
+    if (x,y) not in zauzeti:
+	#ovo provjerava je li polje u sučelju
+            if x > 3:		
+                print('Polje je izvan sučelja, molim unesite drugo polje.')
+                continue
+            elif y > 3:
+                print('Polje je izvan sučelja, molim unesite drugo polje.')
+                continue
+            else:
+                polje[x][y] = igrač
+                zauzeti.add((x,y))
+                potez += 1
+    else:
+        print('Ovo mjesto je zauzeto, unesi novu koordinatu')
+        continue
+	
 	# provjeravanje uvjeta za pobjedu
 	if len(set(polje[x])) == 1:
 		potez = 11
